b2b50f10cc87f8ae9799020d2abf1e1d7741e557
tracer-ysyx <tracer@ysyx.org>
2025-10-07 17:42:15 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:15 up  5:43,  3 users,  load average: 0.22, 0.05, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -47,15 +47,18 @@ static int cmd_c(char *args) {
   return 0;
 }
 
-
 static int cmd_q(char *args) {
-  //add
+  //add next line
   nemu_state.state = NEMU_QUIT;
-
   return -1;
 }
 
 static int cmd_help(char *args);
+
+static int cmd_si(char *args) {
+  	cpu_exec(*args - '0');
+	return 0;	
+}
 
 static struct {
   const char *name;
@@ -65,6 +68,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Execute n instructions, the default value of n is 1", cmd_si },
 
   /* TODO: Add more commands */
 
