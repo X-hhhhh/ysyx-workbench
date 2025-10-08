7466ee2ce5901aad23b04d671ce3626ee70bc9d2
tracer-ysyx <tracer@ysyx.org>
2025-10-08 21:12:52 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:52 up  5:33,  1 user,  load average: 0.04, 0.03, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -127,10 +127,13 @@ static bool make_token(char *e) {
 			strncpy(tokens[nr_token].str, substr_start, substr_len);	//record data values
 			tokens[nr_token].str[substr_len] = '\0';			//add '\0' to make sure it is a string
 			break;
-		default: TODO();
+		default:
+
         }
 	
-	nr_token++;
+	if(rules[i].token_type != TK_NOTYPE) {
+		nr_token++;
+	}
 
         break;
       }
