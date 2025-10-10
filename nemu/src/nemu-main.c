50fdfb5bbe6bcc7595b0c7ab7af0fdde9253ac16
tracer-ysyx <tracer@ysyx.org>
2025-10-10 13:58:43 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:58:43 up 50 min,  1 user,  load average: 0.06, 0.03, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -32,8 +32,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  	//char str[32] = "(20 - (2 + 1) * (2) + 2)";	
-	char str[32] = "((2+3) * (1 +3*(2)))";
+  	char str[32] = "(20 - (2 + 1) * (2) + 2)";	
   	bool success;
 
 	printf("str=%s\n", str);
