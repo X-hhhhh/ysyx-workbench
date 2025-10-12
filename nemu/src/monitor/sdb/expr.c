fae16f07ea0af9c59818d47ba54bc8390f57af5d
tracer-ysyx <tracer@ysyx.org>
2025-10-12 13:37:25 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:37:25 up  1:21,  1 user,  load average: 0.06, 0.05, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -274,7 +274,7 @@ static uint32_t eval(int p, int q, bool *valid) {
 					}else {
 						if(compare_priority(tokens[i].type, tokens[main_op_pos].type) == 1) {
 							switch(tokens[i].type) {
-								case '+': case '-': case '*': case '/':
+								case '+': case '-': case '*': case '/': case TK_EQ:
 									main_op_pos = i; break;
 								case TK_NEG: case TK_DEREF:
 									if(tokens[main_op_pos].type != TK_NEG && tokens[main_op_pos].type != TK_DEREF) {
