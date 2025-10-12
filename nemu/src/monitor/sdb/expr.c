3fb4ea92deabfafec9f8d7c2014720a45f9ed4ee
tracer-ysyx <tracer@ysyx.org>
2025-10-12 13:54:21 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:21 up  1:38,  1 user,  load average: 0.04, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -117,6 +117,9 @@ static bool make_token(char *e) {
 		case TK_EQ:
 			tokens[nr_token].type = TK_EQ;
 			break;
+		case TK_NEQ:
+			tokens[nr_token].type = TK_NEQ;
+			break;
 		case '*':
 			tokens[nr_token].type = '*';
 			break;
