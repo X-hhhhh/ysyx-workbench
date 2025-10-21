f27f9b8b96a8b3609e1093ffd261cd79a74afbaf
tracer-ysyx <tracer@ysyx.org>
2025-10-21 20:52:21 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:21 up  3:31,  4 users,  load average: 0.02, 0.01, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,13 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t i = 0;
+	while(src[i] != '\0') {
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +23,28 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t i = 0;
+	while(dst[i] != '\0') {
+		i++;	
+	}
+	while(*src != '\0') {
+		dst[i++] = *src;
+		src++;
+	}
+	dst[i] = '\0';
+	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	while(*s1 != '\0' && *s2 != '\0') {
+		if(*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}	
+	if(*s1 == *s2) return 0;
+	return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +52,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	unsigned char uc = (unsigned char)n;
+	unsigned char *p = s;
+	for(int i = 0; i < n; i++) {
+		p[i] = uc;
+	}
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +69,13 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+	for(int i = 0; n > 0; n--) {
+		i = *(char*)s1 - *(char*)s2;
+		if(i) {return i;}
+		s1 = (char*)s1 + 1;
+		s2 = (char*)s2 + 1;
+	}
+	return 0;
 }
 
 #endif
