0eebec1740bbdd1cdd41d2531699125116299976
tracer-ysyx <tracer@ysyx.org>
2025-10-22 16:42:26 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:26 up 10 min,  4 users,  load average: 0.06, 0.05, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,35 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static void reverse_string(char *str) {
+	int len = strlen(str);
+	char temp;
+	for(int i = 0; i < len / 2; i++) {
+		temp = str[len - 1 - i];
+		str[len - 1 - i] = str[i];
+		str[i] = temp;
+	}
+}
+
+static char* int2char(int num, char *buffer) {
+	int is_negative = 0;
+	char *dst = buffer;
+	if(num < 0) {is_negative = 1; num = -num;}
+	if(num == 0) {*dst = '0'; dst++; *dst = '\0'; return buffer;}
+
+	while(num > 0) {
+		*dst = '0' + num % 10;
+		num /= 10;
+		dst++;
+	}
+	
+	if(is_negative) {*dst = '-'; dst++;}
+
+	*dst = '\0';
+	reverse_string(buffer);
+	return buffer;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +43,38 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list args;
+	va_start(args, fmt);
+	int perc = 0;
+	int count = 0;
+	int i = 0;
+	char buf[50];
+	for(; *fmt != '\0'; fmt++) {
+		if(perc == 1) {
+			switch(*fmt) {
+				case 'd': 
+					int2char(va_arg(args, int), buf);
+					for(i = 0; buf[i] != '\0'; i++) {
+						out[count++] = buf[i++];
+					}
+					break;
+				case 's': 
+					strcpy(buf, va_arg(args, char*));
+					for(i = 0; buf[i] != '\0'; i++) {
+						out[count++] = buf[i++];
+					} 
+					break;
+				default: break;
+			}
+			perc = 0;
+		}else if(*fmt == '%') {
+			perc = 1;
+		}else {
+			out[count++] = *fmt;
+		}
+	}
+	va_end(args);	
+	return count;	
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
