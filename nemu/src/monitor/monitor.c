e2c38d1366530d9dff395dd29f3da99a628d82d2
tracer-ysyx <tracer@ysyx.org>
2025-10-25 22:45:18 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:45:18 up  7:33,  5 users,  load average: 0.25, 0.06, 0.02

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -211,8 +211,11 @@ void Ftrace(uint32_t pc, uint32_t dnpc, uint8_t inst_type, uint32_t inst) {
 void Ftrace_report() {
 	printf("Ftrace:\n");
 	for(int i = 0; i < func_call_info.count; i++) {
-		if(func_call_info.type[i] == 'c')
-		printf("%x:  call %s\n", func_call_info.pc[i], func_call_info.info[i]);
+		if(func_call_info.type[i] == 'c') {
+			printf("%x:  call %s\n", func_call_info.pc[i], func_call_info.info[i]);
+		}else {
+			printf("%x:  ret  %s\n", func_call_info.pc[i], func_call_info.info[i]);
+		}
 	}
 	printf("\n");
 }
