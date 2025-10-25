7e8398afb99b00df50dc719f3d9b11c7b7edc626
tracer-ysyx <tracer@ysyx.org>
2025-10-25 22:57:34 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:57:34 up  7:46,  5 users,  load average: 0.04, 0.01, 0.00

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ CFLAGS    += -I$(AM_HOME)/am/src/platform/nemu/include
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt -f ~/ysyx-workbench/am-kernels/tests/cpu-tests/build/add-riscv32-nemu.elf
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt -f ~/ysyx-workbench/am-kernels/tests/cpu-tests/build/recursion-riscv32-nemu.elf
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
