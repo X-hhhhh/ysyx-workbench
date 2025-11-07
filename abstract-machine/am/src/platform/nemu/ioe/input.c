694d4306671b7021f876e20415c067b5c5c8c17a
tracer-ysyx <tracer@ysyx.org>
2025-11-07 21:48:58 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:48:58 up  5:10,  5 users,  load average: 0.05, 0.07, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,19 +7,15 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	int keycode = (int)inl(KBD_ADDR);
-	/*if(keycode == AM_KEY_NONE) {
+	if(keycode == AM_KEY_NONE) {
 		kbd -> keydown = false;
 		kbd -> keycode = AM_KEY_NONE;
 	}else if((keycode & KEYDOWN_MASK) == 0x8000) {
 		kbd -> keydown = true;
 		kbd -> keycode = keycode & ~KEYDOWN_MASK;
+		printf("%d\n", keycode);
 	}else {
 		kbd -> keydown = false;
 		kbd -> keycode = keycode;
-	}*/
-	if(keycode == AM_KEY_NONE) {
-		
-	}else {
-		printf("%d\n", keycode);
 	}
 }
