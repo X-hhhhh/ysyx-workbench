9eeaa89517a5f26b51c1a8bf9256a668b23d942a
tracer-ysyx <tracer@ysyx.org>
2025-11-09 17:26:45 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:45 up  5:27,  6 users,  load average: 0.01, 0.04, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -36,13 +36,13 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	if(sbuf_size == -1) {sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);}
 	
 	int count = inl(AUDIO_COUNT_ADDR);
-	int free = sbuf_size - count - 1;		//the queue sacrifices a space
-	int datanum = ctl->buf.end - ctl->buf.start;
+	volatile int free = sbuf_size - count - 1;		//the queue sacrifices a space
+	volatile int datanum = ctl->buf.end - ctl->buf.start;
 	int sbuf_rear = inl(AUDIO_SBUF_REAR);
 	printf("sbuf_size=%d, count=%d, free=%d, datanum=%d, sbuf_rear=%d\n", sbuf_size, count, free, datanum, sbuf_rear);
 	
 	//if the space of sbuf is not enough to write, wait until it is enough
-	while(datanum > free) {}
+	while(datanum > free);
 	while(ctl->buf.start < ctl->buf.end) {
 		outl(AUDIO_SBUF_ADDR + sbuf_rear, *(uint8_t *)ctl->buf.start);
 		sbuf_rear = (sbuf_rear + 1) % sbuf_size;
