195122a29816d2a49e237fa96c112ca8a09c2c8a
tracer-ysyx <tracer@ysyx.org>
2025-11-19 22:25:08 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:08 up 37 min,  5 users,  load average: 0.12, 0.05, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -68,7 +68,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
+
+  printf("nemu:\n");
   isa_reg_display();
+
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
