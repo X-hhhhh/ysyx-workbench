d3e2ea9e061417ce758e2ede1e7d2492fbf3113d
tracer-ysyx <tracer@ysyx.org>
2025-11-20 22:06:01 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:01 up  1:14,  6 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/module/EXU.v
+++ npc/vsrc/module/EXU.v
@@ -2,18 +2,40 @@ module 	EXU(
 	input	wire	[31:0]	gpr_rdata1_in,
 	input	wire	[31:0]	gpr_rdata2_in,
 	input	wire	[31:0]	imm,
-	input	wire	[1:0]	EXU_mode,
+	input	wire	[1:0]	EXU_mode, 	//bit[0]:0 for src1 and src2, 1 for src and imm, bit[1]: 0 for add, 1 for sub
 
 	output	reg	[31:0]	EXU_data
 );
 
+wire	[31:0]	a;
+wire	[31:0]	b;
+
 always@(*) begin
 	case(EXU_mode)
-		2'b00: EXU_data = gpr_rdata1_in + gpr_rdata2_in; 
-		2'b01: EXU_data = gpr_rdata1_in + imm;
-		default: EXU_data = 32'b0;
+		2'b00:
+			a = gpr_rdata1_in;
+			b = gpr_rdata2_in;
+		2'b01:
+			a = gpr_rdata1_in;
+			b = imm;
+		//2'b00: EXU_data = gpr_rdata1_in + gpr_rdata2_in; 
+		//2'b01: EXU_data = gpr_rdata1_in + imm;
+		default: 
+			//EXU_data = 32'b0;
+			a = 32'b0;
+			b = 32'b0;
 	endcase
 end
+
+universal_adder universal_adder_inst
+#(
+	.DATAWIDTH = 32
+)
+(
+	.a(a),
+	.b(b),
+	.out(EXU_data)
+);
 
 endmodule
 
---
+++ npc/vsrc/module/universal_adder.v
@@ -0,0 +1,15 @@
+module universal_adder
+#(
+	parameter	DATAWIDTH = 32
+)
+(
+	input	wire	[DATAWIDTH - 1:0] a,
+	input	wire	[DATAWIDTH - 1:0] b,
+	
+	output	wire	[DATAWIDTH - 1:0] out
+);
+
+assign out = a + b;
+
+endmodule
+
