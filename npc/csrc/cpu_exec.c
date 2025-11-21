b8f85bc0997193453b90a978d716db4bc803d231
tracer-ysyx <tracer@ysyx.org>
2025-11-21 17:51:23 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:51:23 up  3:36,  5 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/cpu_exec.c
+++ npc/csrc/cpu_exec.c
@@ -86,7 +86,7 @@ void cpu_exec(uint64_t n) {
 			assert_fali_msg();
 			printf("npc: " ANSI_FG_RED "ABORT " ANSI_NONE "at pc = %x\n", npc_state.halt_pc);
 			printf("Executed instructions: %d\n", inst_num); 
-			assert(0);
+			Assert(0, "");
 			break;
 	}
 }
