d9bbfa4279794852d6fe42c36d99dac4f1588bf7
tracer-ysyx <tracer@ysyx.org>
2025-11-21 17:45:13 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:13 up  3:30,  5 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/module/EXU.v
+++ npc/vsrc/module/EXU.v
@@ -13,25 +13,35 @@ module 	EXU(
 reg	[31:0] 	a;
 reg	[31:0] 	b;
 wire	[31:0] 	out;
+wire		mode;
 wire	       	carry;
+wire		overflow;
 
 always@(*) begin
 	case(EXU_mode)
 		4'b0000: begin
 			a = gpr_rdata1_in;
 			b = gpr_rdata2_in;
+			mode = 1'b0;
 			EXU_data = out;
 		end
 		4'b0001: begin
 			a = gpr_rdata1_in;
 			b = imm;
+			mode = 1'b0;
 			EXU_data = out;
 		end
 		4'b0100: begin
 			//if equal, EXU_data is 0, if a > b, EXU_data is 32'10, if a < b, EXU_data is 32'b100
 			a = gpr_rdata1_in;
-			b = ~gpr_rdata2_in + 1'b1;
+			b = gpr_rdata2_in;
+			mode = 1'b1;
 			EXU_data = (out == 32'b0) ? 32'b0 : (carry == 0) ? 32'b100 : 32'b10;
+		end
+		4'b1000: begin
+			a = gpr_rdata1_in;
+			b = gpr_rdata2_in;
+			EXU_data = (out == 32'b0) ? 32'b0 : (out[31] ^ overflow) ? 32'b100: 32'b10
 		end
 		default: begin 
 			a = 32'b0;
@@ -49,8 +59,10 @@ universal_adder_inst
 (
 	.a(a),
 	.b(b),
+	.mode(mode),
 	.out(out),
-	.carry(carry)
+	.carry(carry),
+	.overflow(overflow)
 );
 
 endmodule
--- npc/vsrc/module/universal_adder.v
+++ npc/vsrc/module/universal_adder.v
@@ -3,14 +3,20 @@ module universal_adder
 	parameter	DATAWIDTH = 32
 )
 (
-	input	wire	[DATAWIDTH - 1:0] a,
-	input	wire	[DATAWIDTH - 1:0] b,
-	
-	output	wire	[DATAWIDTH - 1:0] out,
-	output	wire			  carry
+	input	wire	[DATAWIDTH - 1:0] 	a,
+	input	wire	[DATAWIDTH - 1:0] 	b,
+	input	wire				mode,	//0:add, 1:sub
+						
+	output	wire	[DATAWIDTH - 1:0] 	out,
+	output	wire			  	carry,
+	output	wire				overflow
 );
 
-assign {carry, out} = a + b;
+wire	b_1comp;
+
+assign 	b_1comp = {DATAWIDTH{mode}} ^ b;	//complement of 1
+assign {carry, out} = a + b + mode;
+assign overflow = (a[DATAWIDTH-1] == b_1comp[DATAWIDTH-1]) && (out[DATAWIDTH-1] != a[DATAWIDTH-1]);
 
 endmodule
 
