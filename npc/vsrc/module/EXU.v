16a13fa221443b83e929c646fb37c9270b0ab73a
tracer-ysyx <tracer@ysyx.org>
2025-11-21 18:16:03 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:03 up  4:01,  4 users,  load average: 0.00, 0.01, 0.00

--- npc/vsrc/module/EXU.v
+++ npc/vsrc/module/EXU.v
@@ -17,36 +17,33 @@ wire	[31:0] 	out;
 wire	       	carry;
 wire		overflow;
 
+assign a = gpr_rdata1_in;
+
 always@(*) begin
-	case(EXU_mode)
-		4'b0000: begin
-			a = gpr_rdata1_in;
-			b = gpr_rdata2_in;
+	if(EXU_mode[0] == 1'b0)
+		b = gpr_rdata2_in;
+	else
+		b = imm;
+	endcase
+end
+
+always@(*) begin
+	case(EXU_mode[3:1])
+		3'b000: begin
 			mode = 1'b0;
 			EXU_data = out;
 		end
-		4'b0001: begin
-			a = gpr_rdata1_in;
-			b = imm;
-			mode = 1'b0;
-			EXU_data = out;
-		end
-		4'b0100: begin
+		3'b010: begin
 			//if equal, EXU_data is 0, if a > b, EXU_data is 32'10, if a < b, EXU_data is 32'b100
-			a = gpr_rdata1_in;
-			b = gpr_rdata2_in;
 			mode = 1'b1;
 			EXU_data = (out == 32'b0) ? 32'b0 : (carry == 0) ? 32'b100 : 32'b10;
 		end
-		4'b1000: begin
-			a = gpr_rdata1_in;
-			b = gpr_rdata2_in;
+		3'b100: begin
+			//if equal, EXU_data is 0, if a > b, EXU_data is 32'10, if a < b, EXU_data is 32'b100
 			mode = 1'b1;
 			EXU_data = (out == 32'b0) ? 32'b0 : (out[31] ^ overflow) ? 32'b100: 32'b10;
 		end
 		default: begin 
-			a = 32'b0;
-			b = 32'b0;
 			mode = 1'b0;
 			EXU_data = 32'b0;
 		end
