49c160720174e33357f1dbd24e44a72266f1cbc1
tracer-ysyx <tracer@ysyx.org>
2025-11-22 17:29:37 +0800

    > compile NEMU ysyx_666 majiaqi Linux x-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:29:37 up  5:14,  5 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/pmem.c
+++ npc/csrc/pmem.c
@@ -112,7 +112,7 @@ int pmem_wr(int paddr, int wdata, char wmask) {
 
 //Called by dpi-c
 int pmem_read(int paddr) {
-	printf("read\n");
+	printf("read, valid=%d\n", top->mem_valid);
 	return pmem_rd_t(paddr);
 }
 
